19d3e4d6c1f6399e907a72ce23c1cf027814191a
jyablonski9 <jyablonski9@gmail.com>
2021-09-17 18:57:47 -0500

    update rsconnect

--- deploy/deploy-shinyapps.R
+++ deploy/deploy-shinyapps.R
@@ -1,6 +1,6 @@
 # deploy/deploy-shinyapps.R
 # usethis::use_build_ignore("deploy")
-install.packages"rstudio/rsconnect", repos = "https://cloud.r-project.org"))
+# install.packages"rstudio/rsconnect", repos = "https://cloud.r-project.org"))
 rsconnect::setAccountInfo(
   Sys.getenv("SHINYAPPS_ACCOUNT"),
   Sys.getenv("SHINYAPPS_TOKEN"),
