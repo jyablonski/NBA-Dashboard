513d483a4b019c390df6a1316de9be6498ee3f7b
jyablonski9 <jyablonski9@gmail.com>
2021-09-21 18:59:27 -0500

    ,

--- global.R
+++ global.R
@@ -19,7 +19,6 @@ library(DBI)
 library(gt)
 library(shinythemes)
 
-print(Sys.getenv('aws_port'))
 # call renv::restore() to build packages
 Sys.setenv (TZ="America/Chicago")
 #### AWS CONNECTION #####
