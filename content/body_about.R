3bc78502ce92c7dc76d8be68b8f7fb65796242b7
BumpVersion Action <bumpversion@github-actions>
2021-11-14 00:53:00 +0000

    Bump version: 1.0.16 → 1.0.17

--- content/body_about.R
+++ content/body_about.R
@@ -35,7 +35,7 @@ body_about <- dashboardBody(
             tags$a(href = "https://github.com/jyablonski", "Github"),
             tags$br(),
             tags$br(),
-            h5("Version: 1.0.16"),
+            h5("Version: 1.0.17"),
             width = 12,
             style = "padding-left: 20px; padding-right: 20px; padding-bottom: 40px; margin-top: -15px;"
           ),
