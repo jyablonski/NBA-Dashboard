e69e6805c4901b4dbe869ab6e0a13062091354b8
jyablonski9 <jyablonski9@gmail.com>
2022-06-03 13:12:26 -0500

    Bump version: 1.2.8 → 1.2.9

--- content/body_about.R
+++ content/body_about.R
@@ -44,7 +44,7 @@ body_about <- dashboardBody(
             tags$a(href = "https://github.com/jyablonski", "Github"),
             tags$br(),
             tags$br(),
-            h5("Version: 1.2.8"),
+            h5("Version: 1.2.9"),
             width = 12,
             style = "padding-left: 20px; padding-right: 20px; padding-bottom: 40px; margin-top: -15px;"
           ),
