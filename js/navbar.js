83e598ad09378f5cc381b91baecdfdcf083b0236
jacob <jyablonski9@gmail.com>
2022-09-17 14:00:59 -0500

    Add docker (#18)
    * new user credentials
    
    * Bump version: 1.3.1 → 1.3.2
    
    * add docker (no multi-stage build)
    
    * add additional reddit plot option
    
    * trim some packages
    
    * fix docs
    
    * add dropdown for APIs
    
    * Bump version: 1.3.2 → 1.4.0

---
+++ js/navbar.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+$(".navbar .container-fluid .navbar-nav .dropdown .dropdown-menu").append('<li><a href="https://graphql.jyablonski.dev" target="_blank">GraphQL API</a></li> <li><a href="https://api.jyablonski.dev" target="_blank">REST API</a></li>');
+  });
